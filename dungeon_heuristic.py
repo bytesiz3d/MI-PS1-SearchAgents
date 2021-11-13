6179ef1a48877539ae04f624cf06f596379cd124
Abdelrahman Farid <abdelrahman.farid99@eng-st.cu.edu.eg>
2021-11-13 09:53:35 +0200

    First path idea

--- dungeon_heuristic.py
+++ dungeon_heuristic.py
@@ -2,7 +2,7 @@ import enum
 from dungeon import DungeonProblem, DungeonState
 from mathutils import Direction, Point, euclidean_distance, manhattan_distance
 from helpers import utils
-from typing import Any, Dict
+from typing import Any, Dict, FrozenSet, List, Tuple
 
 # This heuristic returns the distance between the player and the exit as an estimate for the path cost
 # While it is consistent, it does a bad job at estimating the actual cost thus the search will explore a lot of nodes before finding a goal
@@ -13,47 +13,63 @@ def weak_heuristic(problem: DungeonProblem, state: DungeonState):
 
 
 def strong_heuristic(problem: DungeonProblem, state: DungeonState) -> float:
-    '''
-    - `cache[points]` contains all remaining coins and the goal state
-    '''
+    goal, distance_to_exit = get_current_goal(problem, state.remaining_coins)
+
+    # initial_coins = problem.initial_state.remaining_coins
+    # eaten = state.player
+
+    # # Verify if we're following the path
+    # initial_coins = problem.initial_state.remaining_coins
+    # eaten = state.player
+    # if eaten in initial_coins:
+    #     goal, distance_to_exit = get_current_goal(problem, state.remaining_coins | {eaten})
+
+    #     # If path is being followed, revert check
+    #     if goal == eaten:
+    #         goal, distance_to_exit = get_current_goal(problem, state.remaining_coins)
+
+    # print(state.player, state.remaining_coins, goal)
+    # for rem, (pt, d) in problem.cache().items():
+    #     print(rem, pt, d)
+    # print("-" * 120)
+
+    return manhattan_distance(state.player, goal) + distance_to_exit
+
+
+def get_current_goal(problem: DungeonProblem, remaining_coins: FrozenSet[Point]) -> Tuple[Point, int]:
     cache = problem.cache()
-    if "points" not in cache:
-        cache_init_points(problem, cache)
+    if remaining_coins in cache:
+        return cache[remaining_coins]
 
-        # print(f'$$ {len(problem.initial_state.remaining_coins)} LEN {len(cache["points"])}')
-        # for pt, d in zip(cache["points"], cache["distances"]):
-        #     print(pt, d)
+    points = [problem.layout.exit]
+    distances = [0]
 
-    points = cache["points"]
-    total_points = len(cache["points"])
-    idx = (total_points - 1) - len(state.remaining_coins)
-
-    return manhattan_distance(state.player, points[idx]) + cache["distances"][idx]
-
-
-def cache_init_points(problem: DungeonProblem, cache: Dict[Any, Any]):
-    '''
-    - Sort points by closeness to each other
-    - Form a path ending with the goal
-    '''
-    # Form a path of coins towards the exit
-    cache["points"] = [problem.layout.exit]
-    cache["distances"] = [0]
-    initial_state = problem.get_initial_state()
+    coins = list(remaining_coins)
 
     # Empty coins
-    coins = list(initial_state.remaining_coins)
-    if not len(coins): return
+    if not len(coins):
+        cache[remaining_coins] = (points[0], distances[0])
+        return cache[remaining_coins]
 
     while coins:
+        # Find the closest coin to the last inserted coin
         closest_coin = 0
         closest_coin_dist = problem.layout.width * problem.layout.height
         for i, coin in enumerate(coins):
-            dist = manhattan_distance(cache["points"][0], coin)
+            dist = manhattan_distance(points[0], coin)
             if dist < closest_coin_dist:
                 closest_coin = i
                 closest_coin_dist = dist
 
-        cache["points"].insert(0, coins[closest_coin])
-        cache["distances"].insert(0, closest_coin_dist + cache["distances"][0])
+        # Insert the coin and its distance
+        points.insert(0, coins[closest_coin])
+        distances.insert(0, closest_coin_dist + distances[0])
+
+        # Add this state to cache
+        remaining = frozenset(points[:-1])
+        if remaining not in cache:
+            cache[remaining] = (points[0], distances[0])
+
         del coins[closest_coin]
+
+    return cache[remaining_coins]
